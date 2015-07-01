e4f0d0f8d295edb33caa7e160d390ba29b95a631
mihaigeorgescu <mpgeorgescu@yahoo.com>
2015-07-01 21:53:58 +0300

    hello world achieved! :)

--- src/Bookkeeper/HelloWorldBundle/Controller/DefaultController.php
+++ src/Bookkeeper/HelloWorldBundle/Controller/DefaultController.php
@@ -4,10 +4,9 @@ namespace Bookkeeper\HelloWorldBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('BookkeeperHelloWorldBundle:Default:index.html.twig', array('name' => $name));
-    }
+class DefaultController extends Controller {
+
+	public function indexAction() {
+		return $this->render('BookkeeperHelloWorldBundle:Default:index.html.twig');
+	}
 }
