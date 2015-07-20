c422446dc5fce9b26f2b9f1c8e9aa49500b417c4
Mihai Georgescu <mpgeorgescu@yahoo.com>
2015-07-20 21:45:45 +0300

    ADD action

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -35,7 +35,27 @@ class BookController extends Controller {
 
 
 	public function createAction(Request $request) {
+		$book = new Book();
 
+		$form = $this->createForm(new Booktype(), $book, [
+			'action' => $this->generateUrl('book_create'),
+			'method' => 'POST'
+		]);
+		$form->add('submit', 'submit', ['label' => 'CreateBook']);
+		$form->handleRequest($request);
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($book);
+			$em->flush();
+
+			$this->get('session')->getFlashBag()->add('msg', 'Your book has been created!');
+
+			return $this->redirect($this->generateUrl('book_new'));
+		} else {
+
+			$this->get('session')->getFlashBag()->add('msg', 'Something went wrong!');
+			return $this->render('BookkeeperManagerBundle:Book:new.html.twig', ['form' => $form->createView()]);
+		}
 	}
 
 
