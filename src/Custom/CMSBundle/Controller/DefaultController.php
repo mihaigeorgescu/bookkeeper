95f4ce2674345c3b850dfb5ac7bf4f6ef875a13a
Mihai Georgescu <mpgeorgescu@yahoo.com>
2015-08-01 21:50:13 +0300

    training complete; todo: solve the 'Invalid entities' console message

--- src/Custom/CMSBundle/Controller/DefaultController.php
+++ src/Custom/CMSBundle/Controller/DefaultController.php
@@ -7,6 +7,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller {
 
     public function indexAction() {
-        return $this->render('CustomCMSBundle:Default:index.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+
+    	$pages = $em->getRepository('CustomCMSBundle:Page')->findAll();
+
+        return $this->render('CustomCMSBundle:Default:index.html.twig', ['pages' => $pages]);
+    }
+
+
+    public function displayAction($id) {
+    	$em = $this->getDoctrine()->getManager();
+
+    	$page = $em->getRepository('CustomCMSBundle:Page')->find($id);
+
+        return $this->render('CustomCMSBundle:Default:display.html.twig', ['page' => $page]);
     }
 }
